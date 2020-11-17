2a49f39ce26a85e73b5688806550d2dedd0766cf
VT Vo <vtvo90@outlook.com>
2020-11-17 11:59:06 +0100

    master

--- Ubung04_A04.py
+++ Ubung04_A04.py
@@ -1,72 +1,53 @@
 class Graph:
-     
-    # init function to declare class variables
     def __init__(self, numNodes):
         self.numNodes = numNodes
         self.adj = [[] for i in range(numNodes)]
- 
-    def addEdge(self, v, w):
-        self.adj[v].append(w)
-        self.adj[w].append(v)
     
-    def DFS(self, temp, v, visited):
+    def addEdge(self, src, des):
+        self.adj[src].append(des)
+        self.adj[des].append(src)
+    
+def DFS(self, start, visited):
 
-        visited[v] = True
-        temp.append(v)
- 
-        for i in self.adj[v]:
-            if visited[i] == False:
-                temp = self.DFS(temp, i, visited)
-        return temp
- 	
-    def connectedComponents(self):
-        visited = []
-        cc = []
-        for i in range(self.numNodes):
-            visited.append(False)
-        for v in range(self.numNodes):
-            if visited[v] == False:
-                temp = []
-                cc.append(self.DFS(temp, v, visited))
-            return cc
+    visited[start] = True
+    for i in self.adj[start]:
+        if not visited[i]:
+            DFS(self, i, visited)
 
-    def isconnected(self):
-        c1 = self.connectedComponents()
+def isconnected(self, nmNodes):
+    
+    for i in range(nmNodes):
+        visited = [False]*nmNodes
+        DFS(self, i, visited)
+        for b in visited:
+            if not b:
+                return False
+    return True
 
-        if(c1 != self):
-            return False
-        else:
-            return True
-        
-        for i in range(self.numNodes):
-            
- 
- 
-# Driver Code
 if __name__ == "__main__":
- 
-    g = Graph(6)
-    g.addEdge(1, 0)
-    g.addEdge(0, 1)
-    g.addEdge(2, 3)
-    g.addEdge(3, 2)
-    g.addEdge(5, 2)
-    g.addEdge(2, 5)
+
+    matrix1 = Graph(6)
+    matrix1.addEdge(0, 1)
+    matrix1.addEdge(1, 3)
+    matrix1.addEdge(1, 5)
+    matrix1.addEdge(3, 1)
+    matrix1.addEdge(5, 1)
+
+
 
     matrix2 = Graph(6)
-    matrix2.addEdge(1, 0)
     matrix2.addEdge(0, 1)
     matrix2.addEdge(1, 3)
-    matrix2.addEdge(3, 1)
     matrix2.addEdge(1, 5)
-    matrix2.addEdge(5, 1)
+    matrix2.addEdge(2, 3)
+    matrix2.addEdge(3, 5)
     matrix2.addEdge(4, 5)
-    matrix2.addEdge(5, 4)
-    
-    if g.isconnected() == False:
-        print("success")
+    matrix2.addEdge(5, 0)
+
+    if isconnected(matrix1, 6) == False:
+        print("Success for matrix 1")
         
-    if matrix2.isconnected() == True:
-        print("success")
+    if isconnected(matrix2, 6) == True:
+        print("success for matrix 2")
  
 # This code is contributed by Abhishek Valsan
